d98f47c523ba9f077e82a565fb93c59c31a3f655
EmanZHD <zahidimane1@gmail.com>
2025-01-23 14:17:30 +0100

    last update

--- script.js
+++ script.js
@@ -7,23 +7,33 @@ const canvas = document.querySelector('.canvas')
 const player = document.querySelector('.player')
 const invaders = document.querySelector('.invaders')
 
+let scores = 0
 let moveVer = player.offsetTop
-
 let moveAmount = 10
+
 let playerInitX = canvas.clientWidth / 2 - player.clientWidth / 2
 let playerInitY = canvas.clientHeight - player.clientHeight - 20
+
 let moveHor = playerInitX
+
 console.log('playerInitY : ', playerInitY)
 player.style.transform = `translate(${playerInitX}px)`
+
+let keys = {}
+let moveX = 0
+let moveY = 0
+let reverse = false
+
+let bullets = []
+let bulletYMove = playerInitY
+let canvasREC = canvas.getBoundingClientRect()
 
 function getPlayerXRelativeToCanvas(player, canvas) {
     const playerRect = player.getBoundingClientRect()
     const canvasRect = canvas.getBoundingClientRect()
-
     return playerRect.left - canvasRect.left
 }
 
-let keys = {}
 
 document.addEventListener('keyup', e => {
     keys[e.key] = false
@@ -43,7 +53,7 @@ document.addEventListener('keydown', e => {
         createBullet(playerX)
     }
 })
-let scores = 0
+
 const gameOver = () => {
     canvas.innerHTML = ''
     const done = document.createElement('div')
@@ -61,15 +71,11 @@ const gameOver = () => {
     done.append(score)
     done.append(restart)
     canvas.append(done)
-    restart.addEventListener('click', (event)=>{
-        console.log(event)
+    restart.addEventListener('click', () => {
         canvas.innerHTML = ''
     })
 }
 
-let moveX = 0
-let moveY = 0
-let reverse = false
 const annimateInvaders = () => {
     const enemyRect = invaders.getBoundingClientRect()
     const canvasRect = canvas.getBoundingClientRect()
@@ -81,35 +87,49 @@ const annimateInvaders = () => {
             gameOver()
             return
         case (!reverse && canvasRect.right > enemyRect.right):
-                moveX += 2
+            moveX += 5
             break
         case (!reverse && canvasRect.right == enemyRect.right):
             reverse = true
-            moveY += 23
+            moveY += 53
             break
         case (reverse && canvasRect.left < enemyRect.left):
-            moveX -= 2
+            moveX -= 5
             break
         case (reverse && canvasRect.left == enemyRect.left):
-                reverse = false
-                moveY += 23
+            reverse = false
+            moveY += 53
             break
     }
     // moveX -= 5
     invaders.style.transform = `translate(${moveX}px,${moveY}px)`
     requestAnimationFrame(annimateInvaders)
-    eliminateInvader()
 
 }
-const eliminateInvader = () => {
-    const divs = document.querySelectorAll('.invader')
-console.log('player ',player.getBoundingClientRect())
-    divs.forEach( elem => {
-        console.log('------------------------------>',elem.getBoundingClientRect())
-    })
-//    console.log(`player ${player} \n invqder $
-// {invader}`)
+
+const eliminateInvader = (bullet) => {
+    const enemies = document.querySelector('.invaders')
+    let bombTop = parseInt(bullet.style.top)
+    let invaderBottom = enemies.getBoundingClientRect().bottom
+    console.log('trackBOMB=>', bombTop, 'trackINvader =>',invaderBottom)
+    const enemyRect = enemies ? enemies.getBoundingClientRect() : ' '
+    console.log('enemy----->',enemyRect)
+    if (enemies && bombTop<invaderBottom){
+        enemies.remove()
+    }
+    // if (bullet.length !== 0) {
+    //     bullet.forEach(elem => {
+    //         const bomb = elem.getBoundingClientRect()
+    //         console.log('bomb----->',bomb)
+    //         if (enemies && bomb.top> enemyRect.bottom){
+    //             enemies.remove()
+    //             // gameOver()
+    //             // return
+    //         }
+    //     })
+    // }
 }
+
 const points = () => {
     let i = 0
     while (i < 33) {
@@ -122,38 +142,31 @@ const points = () => {
     annimateInvaders()
 }
 
-
 points()
 
 function test() {
-
     const canvasWidth = canvas.clientWidth
     const playerWidth = player.clientWidth
-
     //player x pos
     const playerX = getPlayerXRelativeToCanvas(player, canvas)
     if (keys['ArrowLeft'] && playerX > 0) {
         moveHor -= moveAmount
     }
-
     if (keys['ArrowRight'] && playerX + playerWidth < canvasWidth) {
         moveHor += moveAmount
     }
-
     requestAnimationFrame(test)
-
     player.style.transform = `translateX(${moveHor}px)`
 }
 
-let bullets = []
-let bulletYMove = playerInitY
-let canvasREC = canvas.getBoundingClientRect()
 
 function moveBullet(bullet) {
     // console.log(canvasREC)
     let bTop = parseInt(bullet.style.top)
+    console.log('bullet TOP', bTop)
     bTop -= moveAmount
     bullet.style.top = `${bTop}px`
+    eliminateInvader(bullet)
     if (bTop < canvasREC.top) {
         bullet.remove()
         ///remove bullet
@@ -161,7 +174,7 @@ function moveBullet(bullet) {
     } else {
         requestAnimationFrame(() => moveBullet(bullet))
     }
-}
+    }
 
 function createBullet(playerX) {
     const bullet = document.createElement('span')
